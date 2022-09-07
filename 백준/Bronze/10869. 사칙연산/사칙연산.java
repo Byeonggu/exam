2b98fc613d669f24fa4db36a3de50b631c20e0ec
Kim Byeongchan8479 <qudcks8749@naver.com>
2022-09-07 22:34:25 +0900

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generateds method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+	
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+		
+	
+	}
+
+}
