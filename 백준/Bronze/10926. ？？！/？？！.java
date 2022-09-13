6ba5d635e5bf2040bf3a1c6a9aca4269a4ded2da
Kim Byeongchan8479 <qudcks8749@naver.com>
2022-09-13 19:47:14 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generateds method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String str = sc.nextLine();
+		
+		System.out.println(str+"??!");
+		
+	
+	}
+
+}
