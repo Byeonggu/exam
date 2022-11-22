91a9bf37e019a9439e515d89159e7428412eae9f
Kim Byeongchan8479 <qudcks8749@naver.com>
2022-11-22 15:47:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= T; i++) {
+
+			for(int j =1; j<=i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+
+	}
+
+}
