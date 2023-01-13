865590b9e6be765f68345e0858095cc0ea41d240
Kim Byeongchan8479 <qudcks8749@naver.com>
2023-01-13 20:45:32 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+class Main {
+	  public static void main(String[] args) throws IOException {
+	      Scanner sc = new Scanner(System.in);
+		  int T;
+	      int a;
+		  T=sc.nextInt();
+	      a=sc.nextInt();
+		System.out.println(T*a);
+			
+	  }
+}
