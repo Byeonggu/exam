f8c29dab1649103aa810f953a4216d8c0597f7f4
Kim Byeongchan8479 <qudcks8749@naver.com>
2023-01-30 21:31:05 +0900

    [Silver V] Title: 숫자 카드, Time: 1328 ms, Memory: 101520 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(Arrays.binarySearch(arr, num)<0) {
+				bw.append("0 ");
+			}else {
+				bw.append("1 ");
+			}
+		
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+	
+}
