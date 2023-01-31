ccbd84b7c5cbbe2375a9c2999e7da7578e9d77de
Kim Byeongchan8479 <qudcks8749@naver.com>
2023-01-31 09:10:02 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double n= sc.nextInt();
+		double a= sc.nextInt();
+		System.out.println(n/a);
+	}
+	
+}
