49f357779429474fcf8cb3dacde87d43ddf99dbe
Kim Byeongchan8479 <qudcks8749@naver.com>
2023-02-17 16:51:21 +0900

    [Silver V] Title: 소트인사이드, Time: 204 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,29 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		String[] str1 = str.split("");
-		int[] arr = new int[str1.length];
-		for(int i=0; i<str1.length; i++) {
-			arr[i]=Integer.parseInt(str1[i]);
+		
+		String num = sc.next();
+		String[] arr = num.split("");
+		int[] arr2 = new int[arr.length];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr2[i] = Integer.parseInt(arr[i]);
 		}
 		
-		for(int i=1; i<str1.length; i++) {
-			if(arr[i-1]<arr[i]) {
-				int j=0;
-				j=arr[i];
-				arr[i]=arr[i-1];
-				arr[i-1]=j;
-				i=0;
+		int idx = 0;
+		int max = 0;
+		int tmp = 0;
+		
+		for(int i=0; i<arr2.length-1; i++) {
+			for(int j=i+1; j<arr2.length; j++) {
+				if(arr2[i] < arr2[j]) {
+					tmp = arr2[i];
+					arr2[i] = arr2[j];
+					arr2[j] = tmp;
+				}
 			}
+			
 		}
-		for(int i: arr) {
-			System.out.print(i);
+		for(int x=0; x<arr2.length; x++) {
+			System.out.print(arr2[x]);
 		}
-		
-
-	}
-}
+	}//
+}//
