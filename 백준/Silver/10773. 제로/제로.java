af2f47c341f77c500592e2353652b6945a971113
Kim Byeongchan8479 <qudcks8749@naver.com>
2023-02-20 15:38:58 +0900

    [Silver IV] Title: 제로, Time: 900 ms, Memory: 108880 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,30 +1,40 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {	
+public class Main {
+	public static void main(String[] args)  {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		Stack<Integer> stack = new Stack<>();
+		int num = 0;
+		int sum = 0;
+		
+		for(int i=1; i<=T; i++) {
+			
+			num = sc.nextInt();
+			if(num == 0) {
+				stack.pop();
+			}else {
+				stack.add(num);
+			}
+			
+		}
+		
+		if(stack.size()==0) {
+			System.out.println(0);
+		}else {
+				for(int s=0; s<stack.size(); s++) {
+					sum += stack.get(s);
+				}
+				System.out.println(sum);
+				
+			}
+		
+		stack.clear();
+		
 
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<Integer>();
-        int N= sc.nextInt();
-        for(int i=0; i<N; i++) {
-        	int num =sc.nextInt();
-        	if(num==0) {
-        		stack.pop();
-        	}else {
-        		stack.push(num);
-        	}
-        	//System.out.println(i);
-        	
-        }
-        //System.out.println(stack);
-        int sum=0;
-        while(!stack.isEmpty()) {
-        	//System.out.println(1);
-        	sum+=stack.pop();
-        }
-        System.out.println(sum);
 	}
-	
-
 }
