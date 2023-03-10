dfc86458e7173493434cadc125ee0f6a3b3b592e
Kim Byeongchan8479 <qudcks8749@naver.com>
2023-03-10 17:38:36 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 124 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+		
+		
+	}
+
+}
