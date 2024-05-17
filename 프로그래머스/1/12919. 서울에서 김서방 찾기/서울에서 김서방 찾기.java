c9fe9072a4ac67d2572ed7e7813f7713293c6f92
Kim Byeongchan8479 <qudcks8749@naver.com>
2024-05-17 20:31:44 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.46 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int ans = 0;
+        for(int i=0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                ans = i;
+            }
+        }
+        return "김서방은 " + ans +"에 있다";
+    }
+}
