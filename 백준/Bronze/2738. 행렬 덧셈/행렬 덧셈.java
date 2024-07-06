5485d59603ee49fc00b77e6bddeb2a4c8a6b6b60
Kim Byeongchan8479 <qudcks8749@naver.com>
2024-07-06 13:38:52 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 224 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,32 +1,33 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[][] arr1= new int[N][M] ;
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				arr1[i][j] = 0;
-			}
-		}
-		
-		
-		for(int z=0; z<2; z++) {
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<M; j++) {
-					arr1[i][j] += sc.nextInt();
-				}
-			}
-		}
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				System.out.print(arr1[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] arr = br.readLine().split(" ");
+        int N = Integer.parseInt(arr[0]);
+        int M = Integer.parseInt(arr[1]);
+
+        int[][] mapA = new int[N][M];
+
+
+        for(int i=0; i<N; i++){
+            String[] input = br.readLine().split(" ");
+            for(int j = 0; j<M; j++){
+                mapA[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+        for(int i=0; i<N; i++){
+            String[] input = br.readLine().split(" ");
+            for(int j = 0; j<M; j++){
+                mapA[i][j] += Integer.parseInt(input[j]);
+                sb.append(mapA[i][j]+" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
 }
