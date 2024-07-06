1bc7118b46576f9ba4f7d73232899a020b56edcf
Kim Byeongchan8479 <qudcks8749@naver.com>
2024-07-06 13:55:12 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15940 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,26 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int maxNum = 0;
+        int x = 0,y=0;
+        for(int i=0; i<9; i++){
+            String[] input = br.readLine().split(" ");
+            for(int j=0; j<9; j++){
+                int tmp = Integer.parseInt(input[j]);
+                if(tmp>=maxNum){
+                    x=i+1; y=j+1;
+                    maxNum= tmp;
+                }
 
-		int[][] arr1= new int[9][9] ;
-		int maxNum=0;
-		int X = 0;
-		int Y = 0;
-		for(int i=0; i<9; i++) {
-			for(int j=0; j<9; j++) {
-				arr1[i][j] = sc.nextInt();
-				if(arr1[i][j]>= maxNum) {
-					maxNum=arr1[i][j];
-					X=i+1;
-					Y=j+1;
-				}
-			}
-		}
-		
-		System.out.println(maxNum);
-		System.out.println(X+" "+Y);
-		
-	}
+            }
+        }
+        System.out.println(maxNum);
+        System.out.println(x+" "+y);
+    }
 }
